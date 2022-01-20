43483faac9a69b2f24eeabe4940124fc89367b9f
aashish nagar <aashish01csp1@gmail.com>
2022-01-21 03:54:03 +0530

    initial commit

---
+++ workflowlearning/workflowlearning/workflowlearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  workflowlearningApp.swift
+//  workflowlearning
+//
+//  Created by Aashish Nagar on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct workflowlearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
